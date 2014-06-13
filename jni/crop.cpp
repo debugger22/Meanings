fb18fc1cdedb83d3ca2640d724f71c241152602e
Sudhanshu Mishra <mrsud94@gmail.com>
2014-06-13 21:51:41 +0530

    Commit updated processing code

--- jni/crop.cpp
+++ jni/crop.cpp
@@ -3,31 +3,38 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <time.h>
 
 using namespace std;
 using namespace cv;
 
 int main(){
-
-    Mat im_rgb  = imread("imtest.jpg");
     Mat dst;
-    int r = int(im_rgb.rows);
-    int c = int(im_rgb.cols);
+    Mat im_rgb = imread("test3.jpg");  // Load image
+    int r = int(im_rgb.rows);  // Get width of the image
+    int c = int(im_rgb.cols);  // Get height of the image
     cout<<r<<" "<<c;
     r=r/2;
     c=c/2;
+
+    if(r<c) {
+        im_rgb = im_rgb.t();
+        flip(im_rgb, im_rgb, 1);
+    }
+    // Set position by hit and trial
     int i=r-r/30;
     int j = c-3*c/10;
     int x = j;
     int y = 3*c/10;
+    // Get the cropping rectangle
     Rect rect = Rect( j, i, x, y );
     cout<<i<<" "<<j<<" "<<x<<" "<<y;
     Mat im_gray;
     im_rgb(rect).copyTo(dst);
     cvtColor(dst,im_gray,CV_RGB2GRAY);
 
-    Mat img_bw = im_gray > 100;
+    Mat img_bw = im_gray > 110;
+    Mat img_dil;
+    dilate(img_bw, img_dil, Mat(), Point(-1, -1), 2, 1, 1);
     imwrite("cropped.jpg", dst);
     imwrite("image_bw.jpg", img_bw);
     return 0;
