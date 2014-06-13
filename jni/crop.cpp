86eee379834cf2a55d0c91a6d032831a8b142add
sarwarc <chahal.sarwar98@gmail.com>
2014-06-13 22:33:58 +0530

    enhanced rectangle dimensions

--- jni/crop.cpp
+++ jni/crop.cpp
@@ -7,24 +7,26 @@
 using namespace std;
 using namespace cv;
 
-int main(){
+int main()
+{
     Mat dst;
-    Mat im_rgb = imread("test3.jpg");  // Load image
+    Mat im_rgb = imread("te.jpg");  // Load image
     int r = int(im_rgb.rows);  // Get width of the image
     int c = int(im_rgb.cols);  // Get height of the image
-    cout<<r<<" "<<c;
-    r=r/2;
-    c=c/2;
+    cout<<r <<" "<<c;
+    r = r/2;
+    c = c/2;
 
-    if(r<c) {
+    if(r < c) 
+    {
         im_rgb = im_rgb.t();
         flip(im_rgb, im_rgb, 1);
     }
     // Set position by hit and trial
-    int i=r-r/30;
-    int j = c-3*c/10;
+    int i = 25*r/27;
+    int j = 2*c/3;
     int x = j;
-    int y = 3*c/10;
+    int y = 8*r/81;
     // Get the cropping rectangle
     Rect rect = Rect( j, i, x, y );
     cout<<i<<" "<<j<<" "<<x<<" "<<y;
